fee375531d24c509a11a01d11dfb57a678b4ca88
mendel <mendeliangyang@hotmail.com>
2015-07-21 08:43:31 +0800

    websocket service use the  thread pool

--- src/java/webSocket/Decoy.java
+++ src/java/webSocket/Decoy.java
@@ -20,37 +20,36 @@ public class Decoy implements Runnable {
     @Override
     public void run() {
         HashSet<String> msgs = null;
-        while (true) {
-            try {
-                Thread.sleep(4000);
-                //检查jmsqueue是否有新的消息，并且分发处理
-                //将jmsqueue clone一份
-                msgs = JMSQueueMessage.HandelMsgQueue();
-                if (msgs == null) {
-                    continue;
-                }
-                for (String msg : msgs) {
-                    //找到 tableName 和msg 一致的 集合进行查询数据和分发
-                    for (MsgFilterModel msgFilterModel : AssignTrial.pushMap.keySet()) {
-                        Set<Session> sessions = AssignTrial.pushMap.get(msgFilterModel);
-                        if (sessions == null || sessions.isEmpty()) {
-                            System.out.println(msgFilterModel.pushMsgId + "no session,so no reapdata");
-                            //这个key下面没有session 不需要查询数据
-                            continue;
-                        }
-                        if (msgFilterModel.dbTable.equals(msg)) {
-                            //启动新的线程处理该数据变动
+
+        try {
+            //检查jmsqueue是否有新的消息，并且分发处理
+            //将jmsqueue clone一份
+            msgs = JMSQueueMessage.HandelMsgQueue();
+            if (msgs == null) {
+                return;
+            }
+            for (String msg : msgs) {
+                //找到 tableName 和msg 一致的 集合进行查询数据和分发
+                for (MsgFilterModel msgFilterModel : AssignTrial.pushMap.keySet()) {
+                    Set<Session> sessions = AssignTrial.pushMap.get(msgFilterModel);
+                    if (sessions == null || sessions.isEmpty()) {
+                        System.out.println(msgFilterModel.pushMsgId + "no session,so no reapdata");
+                        //这个key下面没有session 不需要查询数据
+                        continue;
+                    }
+                    if (msgFilterModel.dbTable.equals(msg)) {
+                        //启动新的线程处理该数据变动
 //                            Thread reapDataThread = new Thread(new ReapData(msgFilterModel, sessions));
 //                            reapDataThread.start();
-                            //use the thread pool take
-                            common.RSThreadPool.ThreadPoolExecute(new ReapData(msgFilterModel, sessions));
+                        //use the thread pool take
+                        common.RSThreadPool.ThreadPoolExecute(new ReapData(msgFilterModel, sessions));
 
-                        }
                     }
                 }
-            } catch (Exception ex) {
-                common.RSLogger.ErrorLogInfo("Decoy error." + ex.getLocalizedMessage());
             }
+        } catch (Exception ex) {
+            common.RSLogger.ErrorLogInfo("Decoy error." + ex.getLocalizedMessage());
         }
+
     }
 }
